0d7544ab60a2a4096365935a2d12af4a751e6421
pcarioufr <pcoid@pm.me>
2024-11-03 10:14:28 +0100

    lab test routes as their own "lab" Blueprint

--- services/webapp/flask/app/__init__.py
+++ services/webapp/flask/app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from ddtrace import tracer
 
 
+from app.auth import auth, login
+from app.lab import lab
+
 @tracer.wrap()
 def init_app():
 
@@ -10,14 +13,18 @@ def init_app():
                 template_folder='./templates')
     app.config.from_object('config.Config')
 
+    login.init_app(app)
 
     with app.app_context():
 
-        from .routes import ping, test
+        from .routes import ping
         from .routes import room_app, room_api
         from .routes import round_api
         from .routes import room_user_id_api
         from .routes import user_api
         from .routes import qr_code
+
+        app.register_blueprint(auth, url_prefix="/auth")
+        app.register_blueprint(lab,  url_prefix="/lab")
 
         return app
---
+++ services/webapp/flask/app/lab/routes.py
@@ -0,0 +1,16 @@
+import flask
+
+from . import lab # Import the Blueprint from __init__.py
+
+from utils import log
+
+
+@lab.route("/<test>", methods=['GET'])
+def test(test=None):
+
+    if test is None:
+        log.warning("missing test in url")
+        return flask.jsonify(), 400
+    
+
+    return flask.render_template("test/{}.jinja".format(test))
